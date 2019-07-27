ef200b2729213e02ee8fd8263414dd7563874bf8
PrasangaDhungel <dhungel611@gmail.com>
2019-07-27 08:26:58 +0545

    buildings in simulation

---
+++ buildings.py
@@ -0,0 +1,68 @@
+import random
+from data.configuration import INFECTION_SPREAD
+
+class Building(object):
+    """
+    Building in simulation kwargs: id, special (bool), x, y
+    """
+    def __init__(self, world,  **kwargs):
+        """
+        Parameters:
+        """
+        try:
+            self.type = kwargs['type']
+        except:
+            self.type = 'normal'
+
+        self.id = kwargs["id"]
+
+        try:
+            self.special = kwargs['special']
+        except: 
+            self.special = False
+
+        try:            
+            self.x  = kwargs['x']
+            self.y  = kwargs['y']
+        except:
+            self.x  = 0
+            self.y  = 0
+
+        self.occupants = []
+        self.world = world
+        if self.special:
+            try:
+                self.interaction_time = kwargs["interaction_time"]
+            except:
+                self.interaction_time = random.randint(3,6)
+        
+        else:
+            try:
+                self.interaction_time = kwargs['interaction_time']
+            except:
+                self.interaction_time = 30000000
+    def addOccupant(self, entity):
+        self.occupants.append(entity)
+    
+    def dropOccupant(self, entity):
+        for i in range(len(self.occupants)):
+            if self.occupants[i].id == entity.id:
+                self.occupants.pop(i)
+                break
+
+    def interact(self):
+        # print(len(self.occupants), "interacting in building: ", self.id)
+        for item1 in self.occupants:
+            if item1.infected == True:
+                for item2 in self.occupants:
+                    if item2.infected == False:                
+                        item2.infected = random.random() < INFECTION_SPREAD 
+
+    def setType(self, type):
+        self.type = type
+
+    def getType(self):
+        return self.type
+        
+    def setOccupants(self, listOfOccupants):
+        self.occupants = listOfOccupants
